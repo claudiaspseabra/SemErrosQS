ddcadb1cdf145e76531ab3c2573f3be74d03ef74
hulkhongan300 <12calistoafonso@gmail.com>
2024-12-02 09:50:22 +0000

    Fixed another missing files

--- src/main/java/com/example/qsproject/qsproject/repositories/ClassroomRepository.java
+++ src/main/java/com/example/qsproject/qsproject/repositories/ClassroomRepository.java
@@ -1,9 +1,7 @@
 package com.example.qsproject.qsproject.repositories;
 
-import com.example.qsproject.qsproject.Classroom;
 import com.example.qsproject.qsproject.dtos.ClassroomDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassroomRepository extends JpaRepository<Classroom,Long> {
-    boolean existsByTag(String tag);
+public interface ClassroomRepository extends JpaRepository<ClassroomDto,Long> {
 }
